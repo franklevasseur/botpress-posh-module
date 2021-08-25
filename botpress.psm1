f17772ebe1f4b021304b6ebec3b7610c6febbc85
François Levasseur <francois_levasseur@hotmail.com>
2021-08-24 20:09:57 -0400

    added a encryption efs tool

--- botpress.psm1
+++ botpress.psm1
@@ -1,4 +1,7 @@
 $BOT = "C:\\Users\\franc\\Documents-franc\\botpress-root"
+$bp_sql_uri = "postgres://postgres:postgres@localhost:5432/botpress"
+$bp_cache = "C:\Users\franc\botpress"
+$bp_posh = "${PSScriptRoot}\botpress.psm1"
 
 function bot { 
     $script = "
@@ -24,72 +27,115 @@ function bot {
     cd $(node -e $script)
  }
 
-Function yb {
-    Param ([string]$module)
+Function efs() {
+    Param ([string]$command, [string]$file)
 
-    if (($module -eq 'core') -or ($module -eq 'studio') -or ($module -eq 'shared') -or ($module -eq 'admin')) {
-        yarn cmd build:$module
-    }
-    elseif ($module -ne '') {
-        yarn cmd build:modules --m $module
-    }
-    else {
-        yarn build
-    }
+    $full_file_path = ($file | Resolve-Path).path -replace '[\\/]', '\\'
+
+    $script = "
+        const Prompt = require('prompt-password');
+        const crypto = require('crypto');
+        const fs = require('fs');
+        const path = require('path')
+        const PWD_CHAR = '*';
+        const ALGORITHM = 'aes-192-cbc';
+        const SALT = 'salt';
+
+        const command = '$command'
+        const file = path.resolve('$full_file_path')
+
+        const getPwd = () => {
+          return new Prompt({
+            type: 'password',
+            message: 'Enter your password please',
+            name: 'password',
+            mask: (input) => PWD_CHAR + new Array(String(input).length).join(PWD_CHAR),
+          }).run();
+        };
+        const createKeyIv = (pwd) => {
+          const key = crypto.scryptSync(pwd, SALT, 24);
+          const iv = new Uint8Array(16);
+          return { key, iv };
+        };
+        const encrypt = (str, pwd) => {
+          const { key, iv } = createKeyIv(pwd);
+          const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
+          return cipher.update(str, 'utf8', 'hex') + cipher.final('hex');
+        };
+        const decrypt = (str, pwd) => {
+          const { key, iv } = createKeyIv(pwd);
+          const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+          return decipher.update(str, 'hex', 'utf8') + decipher.final('utf8');
+        };
+
+        getPwd().then(pwd => {
+            if (command === 'cat') {
+                try {
+                    const fileContent = fs.readFileSync(file, 'utf8');
+                    const decrypted = decrypt(fileContent, pwd);
+                    console.log(decrypted);
+                    return
+                } catch(err) { throw new Error('Invalid password') }
+            }
+            if (command === 'encrypt') {
+                const fileContent = fs.readFileSync(file);
+                const encrypted = encrypt(fileContent, pwd);
+                fs.writeFileSync(file, encrypted);
+                return
+            }
+            if (command === 'decrypt') {
+                try {
+                    const fileContent = fs.readFileSync(file, 'utf8');
+                    const decrypted = decrypt(fileContent, pwd);
+                    fs.writeFileSync(file, decrypted);
+                    return
+                } catch(err) { throw new Error('Invalid password') }
+            }
+            throw new Error('Invalid command')
+        }).catch(err => console.error(err))
+    "
+    node -e $script
+}
+
+Function yb {
+    yarn build $args
 }
 
 Function ys {
-    Param ([string]$entry_point)
-
-    if ($entry_point -eq 'l') {
-        yarn start lang --dim=300
-    }
-    elseif ($entry_point -eq 'stan') {
-        ys nlu --languageURL=http://localhost:3100 --ducklingURL=http://localhost:8000/ --modelCacheSize=1gb --body-size=900kb --silent
-    }
-    else {
-        yarn start $entry_point $args
-    }
+    yarn start $args
 }
 
 Function yt {
-    Param ([string]$test)
-    yarn test $test $args
+    yarn test $args
 }
 
 Function y {
     yarn $args
 }
 
-
 Function bpconf() {
     Param ([string]$filename)
 
-    if (($filename -eq 'global') -or ($filename -eq '')) {
-        Write-Output "${pwd}\out\bp\data\global\botpress.config.json"
+    if ( $filename -eq "posh" ) {
+        Write-Output $bp_posh
+        return
     }
-    elseif ( $filename -eq "posh" ) {
-        Write-Output "${PSScriptRoot}\botpress.psm1"
-    }
-    else {
-        Write-Output "${pwd}\out\bp\data\global\config\${filename}.json"
-    }
+
+    Write-Output "No config for $filename"
 }
 
-Function bitf() {
-    Param ([string]$command)
-
-    if (($command -eq 'ls') -or ($command -eq 'list') -or ($command -eq '')) {
-        node -e "require('@botpress/bitfan').default.datasets.listFiles().then(x => console.log(x))"
-    }
-    else {
-        Write-Output "${command} is not a valid bitfan command."
-    }
+Function touch() {
+    Param ([string]$file_name)
+    New-Item -ItemType file $file_name
 }
 
 Function redis() {
     docker run -it --rm -p 6379:6379 --name docker-redis redis
 }
 
-Export-ModuleMember -Function bot, yb, ys, yt, y, bpconf, bitf, redis
-Export-ModuleMember -Variable BOT
+Function bpsql() {
+    psql $bp_sql_uri
+}
+
+Export-ModuleMember -Function bot, yb, ys, yt, y, bpconf, bitf, redis, bpsql, touch, efs
+Export-ModuleMember -Variable BOT, bp_sql_uri, bp_cache, $bp_posh
